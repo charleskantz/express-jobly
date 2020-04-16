4f52a26b0f71b259e26ee6823e5147f610efeb37
Charles Kantz <kantz.charles@gmail.com>
2020-04-16 00:54:04 -0700

    Test error resolved, some tests complete for GET routes

--- __tests__/integration/companiesRoutes.test.js
+++ __tests__/integration/companiesRoutes.test.js
@@ -1,44 +1,67 @@
-process.env.NODE_ENV = 'test';
 
 const request = require("supertest");
 const app = require("../../app");
 const db = require("../../db");
 const Company = require("../../models/company");
 
-
+process.env.NODE_ENV = 'test';
 let testCompany;
 
 describe("Company Routes Integration Tests", function() {
 
 
   beforeEach(async function () {
-    console.error("literally anything")
-    // await db.query("DELETE FROM companies");
+    await db.query("DELETE FROM companies");
 
     testCompany = await db.query(`
       INSERT INTO companies (
         handle,
-        name
+        name,
+        num_employees
         )
       VALUES (
-        'test',
-        'test company inc.'
+        'testcompany',
+        'test company inc.',
+        2
         )
-        `);
-
-    console.error("TEST COMPANY ~~~~~~~",testCompany.rows);
+    `);
   });
+
+  // GET routes
 
   it("Should get a list of all companies", async function() {
     const response = await request(app).get('/companies');
-    console.error("RESP BODY ~~", response.body);
-    expect(response.body).toEqual(testCompany.rows);
+
     expect(response.statusCode).toBe(200);
-  })
+    expect(response.body.companies).toHaveLength(1);
+    expect(response.body.companies[0]).toHaveProperty('handle');
+  });
+
+  it("Should search for a company by name", async function() {
+    const response = await request(app).get('/companies?search=test');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.companies).toHaveLength(1);
+    expect(response.body.companies[0]).toHaveProperty('handle');
+
+  });
+
+  it("Should error if given a bad name", async function() {
+    const response = await request(app).get('/companies?search=Nope');
+    expect(response.statusCode).toBe(404);
+    expect(response.error.text).toEqual('{"status":404,"message":"No results for Nope"}');
+
+  });
+
+  it("Should error if min employees > max employees", async function() {
+    const response = await request(app).get('/companies?min_employees=500&max_employees=100');
+    expect(response.statusCode).toBe(400);
+    expect(response.error.text).toEqual('{"status":400,"message":"The minimum employees cannot be more than the maximum employees."}');
+
+  });
 
 });
   
-  // GET routes
   
 
   // POST route
